65b3ec96aa30a45d0ff7c16bdb1e3623c297ef6c
Rober04 <roberto.david.adam04@gmail.com>
2025-03-27 09:39:28 +0100

    solved exercise 5

--- js/exercise-5/script.js
+++ js/exercise-5/script.js
@@ -1,3 +1,10 @@
+class KeyError extends Error{
+    constructor(message){
+        super(message);
+        this.name = "KeyError";
+    }
+}
+
 const testMap = new Map();
 const user = {
     name: "user"
@@ -19,10 +26,72 @@ testMap.set("color", "morado");
 
 console.log(testMap);
 
-const REQUIRED_KEY = "moroso";
-testMap.forEach((value, key) => {
-    if(key === REQUIRED_KEY){
-        console.log(`Mostrar llave: ${key}`);
-        console.log(`Comprobar si existe la key hola: `)
+function showKey(map, key){
+    try{
+        const checkKey = hasKey(map, key);
+        if(checkKey === true){
+            return key;
+        }
+        else{
+            throw new KeyError("Key not found");
+        }
     }
-  });
+    catch(KeyError){
+        return KeyError;
+    }
+}
+
+function hasKey(map, key){
+    const keyStatus = map.has(key);
+    return keyStatus;
+}
+
+function showSize(map){
+    const mapSize = map.size;
+    return mapSize;
+}
+
+function deleteElement(map, key){
+    const checkKey = hasKey(map, key);
+    try{
+        if(checkKey === true){
+            map.delete(key);
+            return `Element with key ${key} has been succesfully deleted`;
+        }
+        else{
+            throw new KeyError("Key not found");
+        }
+    }
+    catch(KeyError){
+        return KeyError;
+    }
+}
+
+function clearMap(map){
+    map.clear();
+    return "Map cleared succesfully";
+}
+
+const showKeyMoroso = showKey(testMap, "moroso");
+const hasKeyHola = hasKey(testMap, "hola");
+const totalMapSize = showSize(testMap);
+
+console.log("testMap with foreach: ")
+testMap.forEach((value, key) => {
+    if(value instanceof Object){
+        const object = JSON.stringify(value);
+        console.log(`${key}: ${object}`);
+    }
+    else{
+        console.log(`${key}: ${value}`);
+    }
+  })
+
+const deleteAgarradoElement = deleteElement(testMap, "agarrado");
+const clearTestMap = clearMap(testMap);
+
+console.log(`Mostrar key moroso: ${showKeyMoroso}`);
+console.log(`Comprobar key hola: ${hasKeyHola}`);
+console.log(`Mostrar tamaño total del mapa: ${totalMapSize}`);
+console.log(`Borrar elemento con key agarrado: ${deleteAgarradoElement}`);
+console.log(`Limpiar mapa: ${clearTestMap}`);
