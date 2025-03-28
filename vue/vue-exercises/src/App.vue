dbe832401a7c7fa37106b2efcfccb40df05489c9
Rober04 <roberto.david.adam04@gmail.com>
2025-03-28 12:14:37 +0100

    solved exercise 2 vue

--- vue/vue-exercises/src/App.vue
+++ vue/vue-exercises/src/App.vue
@@ -1,19 +1,11 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-
+import TheTitle from '@/components/TheTitle.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <TheTitle></TheTitle>
     </div>
   </header>
 
---
+++ vue/vue-exercises/src/components/TheTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h2>Título estático</h2>
+  </div>
+</template>
+
+<style scoped>
+h2 {
+  color: rgb(21, 122, 89);
+  font-size: 2em;
+  border: 0.1em solid;
+}
+</style>
