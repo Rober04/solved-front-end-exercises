813697aa81f2659b4f5f2ecbb194cb65bdad8c59
Rober04 <roberto.david.adam04@gmail.com>
2025-03-28 13:22:19 +0100

    solved exercise 3 vue

---
+++ vue/vue-exercises/src/components/TheCounter.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import TheTitle from './TheTitle.vue'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+function increment() {
+  count.value++
+}
+function decrement() {
+  count.value--
+}
+</script>
+
+<template>
+  <div>
+    <h2>
+      <TheTitle><slot name="propTitle"></slot></TheTitle>
+    </h2>
+    <span>{{ count }}</span>
+    <button @click="increment">Increment</button>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+* {
+  font-size: 1.1em;
+}
+button {
+  margin: 1em;
+}
+</style>
