f649c24024b5ccd873c63ca955f2dc8eb43ec558
Rober04 <roberto.david.adam04@gmail.com>
2025-03-31 20:40:23 +0200

    solved exercise 1 nuxt

---
+++ nuxt/nuxt-exercises/pages/catalogo.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <h1>Catalogo</h1>
+        <p>Welcome to the catalogo page</p>
+        <NuxtLink to="/">
+            <button>Home</button>
+        </NuxtLink>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
