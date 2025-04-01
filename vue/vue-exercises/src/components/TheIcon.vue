4d65d532c991b5e06a87b61dcb331306707ad3d4
Rober04 <roberto.david.adam04@gmail.com>
2025-04-01 11:17:17 +0200

    solved exercise 18 vue

---
+++ vue/vue-exercises/src/components/TheIcon.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import IconFacebook from './icons/IconFacebook.vue';
+import IconTwitter from './icons/IconTwitter.vue';
+
+const socialIcons = ref([
+  { name: "facebook", component: IconFacebook },
+  { name: "twitter", component: IconTwitter },
+]);
+</script>
+
+<template>
+  <div class="home__icon icon">
+    <component v-for="icon in socialIcons" :key="icon.component.name" :is="icon.component" :class="'icon__'+icon.name"></component>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .icon{
+    display: inline;
+
+    &__facebook{
+      width: 2em;
+      height: 2em;
+    }
+    &__twitter{
+      width: 2em;
+      height: 2em;
+    }
+  }
+</style>
---
+++ vue/vue-exercises/src/components/icons/IconTwitter.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="/twitter.svg" alt="icon">
+</template>
